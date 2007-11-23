3e569e6e2387adefa70b7deb86948a10d49367a1
Kolbjørn Fredheim <coolbeer1980@yahoo.com>
2007-11-23 19:21:56 +0100

    main.cpp: Exchanged every tab with 4 spaces

--- src/main.cpp
+++ src/main.cpp
@@ -9,113 +9,113 @@ void showhelp(void);
 int main (int argc, char *argv[])
 {
 #ifdef PWANDEBUG
-	std::cout << PACKAGE_NAME << " v" << PACKAGE_VERSION << "\n\n";
-	std::cout << "Raw Commandline:\n=================================\n";
-	for(int teller = 0; teller != argc; ++teller)
-		std::cout << "argc = " << teller << "; " << argv[teller] << "\n";
-	std::cout << "\n";
+    std::cout << PACKAGE_NAME << " v" << PACKAGE_VERSION << "\n\n";
+    std::cout << "Raw Commandline:\n=================================\n";
+    for(int teller = 0; teller != argc; ++teller)
+        std::cout << "argc = " << teller << "; " << argv[teller] << "\n";
+    std::cout << "\n";
 #endif
-	
-	std::map<std::string, std::string> arguments;
-	QApplication app(argc, argv);
-	arguments = parsecommands(argc, argv);
+
+    std::map<std::string, std::string> arguments;
+    QApplication app(argc, argv);
+    arguments = parsecommands(argc, argv);
 
 #ifdef PWANDEBUG
-	std::cout << "Commandline Arguments parsed:\n=================================\n";
-	std::map<std::string, std::string>::iterator mapiter = arguments.begin();
-	while(mapiter != arguments.end())
-	{
-		std::cout << (*mapiter).first << " : " << (*mapiter).second << "\n";
-		++mapiter;
-	}
-	std::cout << "\n";
+    std::cout << "Commandline Arguments parsed:\n=================================\n";
+    std::map<std::string, std::string>::iterator mapiter = arguments.begin();
+    while(mapiter != arguments.end())
+    {
+        std::cout << (*mapiter).first << " : " << (*mapiter).second << "\n";
+        ++mapiter;
+    }
+    std::cout << "\n";
 #endif
-	
-	t_imageviewer *imageviewer = new t_imageviewer;
-	if(arguments.find("version") != arguments.end())
-	{
-		std::cout << PACKAGE_NAME << " v" << PACKAGE_VERSION << "\n\n";
-		exit(0);
-	}
-	if(arguments.find("help") != arguments.end())
-	{
-		showhelp();
-		exit(0);
-	}
-	if(arguments.find("image") != arguments.end())
-	{
-		if(!imageviewer->startimageviewer(arguments))
-		{
-			std::cout << "Could not load specified file...\n";
-			exit(1);
-		}
-		imageviewer->show();
-		return app.exec();
-	}
-	else
-	{
-		std::cout << PACKAGE_NAME << " v" << PACKAGE_VERSION << "\n";
-		std::cout << "Missing option\nUsage: " << PACKAGE_NAME << " [OPTION]\n\n\
+
+    t_imageviewer *imageviewer = new t_imageviewer;
+    if(arguments.find("version") != arguments.end())
+    {
+        std::cout << PACKAGE_NAME << " v" << PACKAGE_VERSION << "\n\n";
+        exit(0);
+    }
+    if(arguments.find("help") != arguments.end())
+    {
+        showhelp();
+        exit(0);
+    }
+    if(arguments.find("image") != arguments.end())
+    {
+        if(!imageviewer->startimageviewer(arguments))
+        {
+            std::cout << "Could not load specified file...\n";
+            exit(1);
+        }
+        imageviewer->show();
+        return app.exec();
+    }
+    else
+    {
+        std::cout << PACKAGE_NAME << " v" << PACKAGE_VERSION << "\n";
+        std::cout << "Missing option\nUsage: " << PACKAGE_NAME << " [OPTION]\n\n\
 Try: `" << PACKAGE_NAME << " --help` for more options.\n\n";
-		exit(0);
-	}
+        exit(0);
+    }
 }
 
 std::map<std::string, std::string> parsecommands(int argc, char *argv[])
 {
-	std::map<std::string, std::string> retvalue;
-	std::list<std::string> arguments;
-	std::list<std::string>::iterator iter;
-	
-	for (int i = 1; i != argc; i++)
-	{
-		arguments.push_back(argv[i]);
-	}
-	
-	//iter = arguments.begin();
-	for(iter = arguments.begin(); iter != arguments.end(); ++iter)
-	{
-		if((*iter) == "--image" || (*iter) == "-i")
-		{
-			++iter;
-			if(iter != arguments.end())
-				retvalue["image"] = *iter;
-			else
-				--iter;
-		}
-		else if((*iter) == "--scale" || (*iter) == "-s")
-		{
-			retvalue["scale"] = "true";
-		}
-		else if((*iter) == "--version" || (*iter) == "-v")
-		{
-			retvalue["version"] = "true";
-		}
-		else if((*iter) == "--help" || (*iter) == "-h")
-		{
-			retvalue["help"] = true;	
-		}
-	}
-/*	
-	}
-	while (iter != arguments.end())
-	{
-		if((*iter) == "--image" && iter +1 != arguments.end())
-		{
-			retvalue["image"] = *++iter;
-		}
-		if((*iter) == "--scale")
-		{
-			retvalue["scale"] = "true";
-		}
-		if((*iter) == "--version")
-		{
-			retvalue["version"] = "true";
-		}
-		++iter;
-	}
+    std::map<std::string, std::string> retvalue;
+    std::list<std::string> arguments;
+    std::list<std::string>::iterator iter;
+
+    for (int i = 1; i != argc; i++)
+    {
+        arguments.push_back(argv[i]);
+    }
+
+    //iter = arguments.begin();
+    for(iter = arguments.begin(); iter != arguments.end(); ++iter)
+    {
+        if((*iter) == "--image" || (*iter) == "-i")
+        {
+            ++iter;
+            if(iter != arguments.end())
+                retvalue["image"] = *iter;
+            else
+                --iter;
+        }
+        else if((*iter) == "--scale" || (*iter) == "-s")
+        {
+            retvalue["scale"] = "true";
+        }
+        else if((*iter) == "--version" || (*iter) == "-v")
+        {
+            retvalue["version"] = "true";
+        }
+        else if((*iter) == "--help" || (*iter) == "-h")
+        {
+            retvalue["help"] = true;	
+        }
+    }
+/*
+    }
+    while (iter != arguments.end())
+    {
+        if((*iter) == "--image" && iter +1 != arguments.end())
+        {
+            retvalue["image"] = *++iter;
+        }
+        if((*iter) == "--scale")
+        {
+            retvalue["scale"] = "true";
+        }
+        if((*iter) == "--version")
+        {
+            retvalue["version"] = "true";
+        }
+        ++iter;
+    }
 */
-	return retvalue;
+    return retvalue;
 }
 
 void showhelp(void)
