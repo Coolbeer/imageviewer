7a808a6c3468a1825fa3787a710e76d608d86ed6
Kolbjørn Fredheim <hightower@tregt.net>
2009-01-26 15:19:32 +0100

    Beginnings of frontend, added a few new code files

---
+++ src/imageviewer_frontend_base.cpp
@@ -0,0 +1,6 @@
+#include "imageviewer_frontend_base.h"
+
+pwan::imageviewer_frontend_base::~imageviewer_frontend_base(void)
+{
+
+}
---
+++ src/imageviewer_frontend_qt.cpp
@@ -0,0 +1,6 @@
+#include "imageviewer_frontend_qt.h"
+
+void pwan::imageviewer_frontend_qt::show(void)
+{
+    return;
+}
