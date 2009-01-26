f1b644f40dbc42d10798ef1d719bce9bc5e86d0a
Kolbjørn Fredheim <hightower@tregt.net>
2009-01-26 14:09:43 +0100

    New file: imageviewer_frontend_base.h, beginnings of a frontend base class

---
+++ src/imageviewer_frontend_base.h
@@ -0,0 +1,14 @@
+#ifndef IMAGEVIEWER_FRONTEND_BASE_H
+#define IMAGEVIEWER_FRONTEND_BASE_H
+
+namespace pwan
+{
+    class imageviewer_frontend_base
+    {
+        public:
+            virtual void show = 0;
+        private:
+    };
+}
+
+#endif
