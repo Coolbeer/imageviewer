4d3c92112f359ae5332e67c00f57abba1ffca7ce
Kolbjørn Fredheim <hightower@tregt.net>
2009-06-17 02:07:06 +0200

    Added stripped down config.h to please vc++

---
+++ config.h
@@ -0,0 +1,20 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "imageviewer"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "imageviewer"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "imageviewer 0.1.5"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "imageviewer"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.1.5"
+
+/* Version number of package */
+#define VERSION "0.1.5"
