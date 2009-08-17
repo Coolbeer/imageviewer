13d54a7f93063706f7fbb12a5d488501999e9a0a
Kolbjørn Fredheim <hightower@tregt.net>
2009-08-17 14:40:15 +0200

    Some more work on the backend

--- src/imageviewer_backend_qt.cpp
+++ src/imageviewer_backend_qt.cpp
@@ -1,1 +1,13 @@
 #include "imageviewer_backend_qt.h"
+
+int pwan::imageviewer_backend_qt::init()
+{
+        assert(!m_thread);
+        m_thread = boost::shared_ptr<boost::thread>(new boost::thread(boost::bind(&pwan::imageviewer_backend_qt::do_work, this)));
+        return 0;
+}
+
+void pwan::imageviewer_backend_qt::do_work()
+{
+
+}
--- src/imageviewer_backend_qt.h
+++ src/imageviewer_backend_qt.h
@@ -3,4 +3,21 @@
 
 #include "imageviewer_backend_base.h"
 
+#include <boost/shared_ptr.hpp>
+#include <boost/thread.hpp>
+
+
+namespace pwan
+{
+    class imageviewer_backend_qt : public imageviewer_backend_base
+    {
+        public:
+            int init(void);
+        private:
+            void do_work(void);
+            boost::shared_ptr<boost::thread> m_thread;
+            boost::mutex m_mutex;
+    };
+}
+
 #endif
